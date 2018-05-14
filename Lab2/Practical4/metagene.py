7f743c0ff2fa6407e11d4a9b3022c9f3c9cf8449
YouchengZHANG <36500121+YouchengZHANG@users.noreply.github.com>
2018-05-14 20:44:32 +0200

    Add script metagene.py
    Practical 4 - Exercise 2 -​ Metagene Approach
    1. Make a multiple alignment of each of your multi FASTA cluster files in order to have one
    alignment for each cluster of orthologous genes.

---
+++ Lab2/Practical4/metagene.py
@@ -0,0 +1,24 @@
+
+
+
+#  Metagene Extractor and Construction
+#  This script when runs creates 'orth_meatagene.fa' that contains all the aligned gene in all cluster files
+
+s_16 = ''
+s_04 = ''
+s_05 = ''
+s_08 = ''
+
+for i in range(1,11):
+	names = 'orth_' + str(i) + '_k' + '.fa'
+	fo = open(names,'r+')
+	f = fo.read().splitlines()
+	s_16 += str(f[1])
+	s_04 += str(f[3])
+	s_05 += str(f[5])
+	s_08 += str(f[7])
+	fo.close()
+
+fm = open('orth_metagene.fa','w')
+fm.write('>16' + '\n' + s_16 + '\n' + '>04' + '\n' + s_04 + '\n' + '>05' + '\n' + s_05 + '\n' + '>08' + '\n' + s_08 + '\n')
+fm.close()
