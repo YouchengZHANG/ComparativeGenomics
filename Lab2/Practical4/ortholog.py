27c3c1912917df0203bb60d8dc81f93249ef766b
YouchengZHANG <36500121+YouchengZHANG@users.noreply.github.com>
2018-05-14 21:03:34 +0200

    Update ortholog.py

--- Lab2/Practical4/ortholog.py
+++ Lab2/Practical4/ortholog.py
@@ -6,7 +6,6 @@
 #  secondly selects 10 different ortholog clusters and extracts the corresponding protein sequences, 
 #  then saves each ortholog cluster into 10 separated fasta files, named 'orth_01.fa', 'orth_02.fa', ... , 'orth_10.fa'
 
-
 Parse01 = open('16_04_parse.txt','r+')
 Parse02 = open('16_05_parse.txt','r+')
 Parse03 = open('16_08_parse.txt','r+')
@@ -29,16 +28,21 @@ ref3 = [l3[i][0] for i in range(len(f2))]
 db3 = [l3[i][2] for i in range(len(f2))]
 #print(len(ref1),len(ref2),len(ref2))
 
-#orth_cluster = [x for x in ref1 if x in ref2 and x in ref3]
+orth_cluster = [x for x in ref1 if x in ref2 and x in ref3]
 #print(len(orth_cluster))
 
 #  Create 'orth_cluster.txt' file
-#cluster = open('orth_cluster.txt','w')
-#for i in range(len(orth_cluster)):
-#	cluster.write(orth_cluster[i] + ' ' + db1[ref1.index(orth_cluster[i])] + ' ' + db2[ref2.index(orth_cluster[i])] + ' ' + db3[ref3.index(orth_cluster[i])] + '\n')
-#cluster.close()
+cluster = open('orth_cluster.txt','w')
+for i in range(len(orth_cluster)):
+	cluster.write(orth_cluster[i] + ' ' + db1[ref1.index(orth_cluster[i])] + ' ' + db2[ref2.index(orth_cluster[i])] + ' ' + db3[ref3.index(orth_cluster[i])] + '\n')
+cluster.close()
 
+
+
+#  Cluster Genome Extractor
+#  This script when runs creates 10 cluster fasta files with ortholog full sequences in each species
 #  Select 10 different ortholog clusters and gather corresponding sequences to 10 separated files
+
 Prot01 = open('04_p.fa','r+')
 Prot02 = open('05_p.fa','r+')
 Prot03 = open('08_p.fa','r+')
@@ -55,6 +59,5 @@ for k,i in enumerate(orth_select):
 	names = 'orth_' + str(int(k)+1)
 	vars = open(names,'w')
 	vars.write(i + '\n' + p4[int(p4.index(i))+1] + '\n' + db1[ref1.index(i)] + '\n' + p1[p1.index(db1[ref1.index(i)])+1] + '\n' + db2[ref2.index(i)] + '\n' + p2[p2.index(db2[ref2.index(i)])+1] + '\n' + db3[ref3.index(i)] + '\n' + p3[p3.index(db3[ref3.index(i)])+1] + '\n')	
-
 
 
