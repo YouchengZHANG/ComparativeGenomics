12ae8b78997349d559967e1fff42a8bc476e5538
YouchengZHANG <36500121+YouchengZHANG@users.noreply.github.com>
2018-05-18 20:30:39 +0200

    Add script getGeneOrder.py
    Practical5 - Exercise 2 - Gene order
    1. We provide the script getGeneOrder.py which reads a proteome multi FASTA file (i.e.,
    01.fa.txt.pfa) and a cluster file from Practical 4

---
+++ Lab3/Practical5/getGeneOrder.py
@@ -0,0 +1,53 @@
+import sys, re
+
+# acquire first needed data
+
+geneOrderList = []
+
+aHandle = open (sys.argv [1])
+
+lines = aHandle.readlines ()
+
+for aLine in lines:
+
+	aLine = aLine.replace ("\n", "")
+
+	if aLine.startswith (">"):
+
+		#print aLine [0:len (aLine)],
+		geneOrderList.append (aLine [0:len (aLine)])
+#print geneOrderList
+
+# acquire second needed data
+
+partOfCluster = {}
+
+bHandle = open (sys.argv [2])
+
+lines = bHandle.readlines ()
+
+id = 0
+
+for aLine in lines:
+
+	aLine = aLine.replace ("\n", "")
+	#print aLine
+	#words = aLine.rstrip('\r').split ("\t")
+	words = aLine.split (" ")
+	#print words
+	for aWord in words:
+
+		if not partOfCluster.has_key (aWord):
+
+			partOfCluster [aWord] = id
+
+	id = id + 1
+
+#print partOfCluster
+# put together
+
+for aGene in geneOrderList:
+
+	if partOfCluster.has_key (aGene):
+
+		print partOfCluster [aGene],
